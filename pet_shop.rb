55550bbf22bf5966759932556b554ac405d7af5f
Alex Fedenczuk <alex.fedenczuk@gmail.com>
2018-02-02 13:53:09 +0000

    Added methods for returning the name, cash, number of pets sold, stock count, adding or removing cash, increasing the number of pets sold and searching pets by breed or name.

--- pet_shop.rb
+++ pet_shop.rb
@@ -7,6 +7,36 @@ def total_cash(pet_shop)
 end
 
 def add_or_remove_cash(pet_shop, credit_or_deposit)
-  total = (pet_shop[:admin][:total_cash] + credit_or_deposit)
-  return total
+  pet_shop[:admin][:total_cash] = (pet_shop[:admin][:total_cash] + credit_or_deposit)
+end
+
+def pets_sold(pet_shop)
+  return pet_shop[:admin][:pets_sold]
+end
+
+def increase_pets_sold(pet_shop, number_of_pets_sold)
+  pet_shop[:admin][:pets_sold] = (pet_shop[:admin][:pets_sold] + number_of_pets_sold)
+end
+
+def stock_count(pet_shop)
+  return pet_shop[:pets].count
+end
+
+def pets_by_breed(pet_shop, breed_to_be_found)
+  pets_found = []
+  for pet in pet_shop[:pets]
+    if pet[:breed] == breed_to_be_found
+       pets_found.push(pet)
+     end
+  end
+  return pets_found
+end
+
+def find_pet_by_name(pet_shop, name)
+  for pet in pet_shop[:pets]
+    if pet[:name] == name
+      return pet
+    end
+  end
+  return nil
 end
